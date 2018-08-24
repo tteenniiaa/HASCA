ab65d0482289c4aa216f1b53b62d5cb10a598415
Tenia <tenia.rosalina@gmail.com>
2018-08-24 09:32:00 -0700

    initial commit

--- app/src/main/java/h/tenia/humansmokingcapacity/Mainmenu.java
+++ app/src/main/java/h/tenia/humansmokingcapacity/Mainmenu.java
@@ -1,9 +1,10 @@
 package h.tenia.humansmokingcapacity;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Mainmenu extends AppCompatActivity {
+public class Mainmenu extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
