75252bc760f0ca13bc9dc0491382d5af3abb1f08
Tenia <tenia.rosalina@gmail.com>
2018-08-24 10:50:44 -0700

    initial commit

--- app/src/main/java/h/tenia/humansmokingcapacity/Mainmenu.java
+++ app/src/main/java/h/tenia/humansmokingcapacity/Mainmenu.java
@@ -1,8 +1,11 @@
 package h.tenia.humansmokingcapacity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Mainmenu extends Activity {
 
@@ -10,5 +13,41 @@ public class Mainmenu extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
+
+        final LinearLayout btnPeriksa = (LinearLayout) findViewById(R.id.btnPeriksa);
+        final LinearLayout btnHasil = (LinearLayout) findViewById(R.id.btnHasil);
+        final LinearLayout btnTtg = (LinearLayout) findViewById(R.id.btnTtg);
+
+        btnTtg.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View argo) {
+                                              //TODO Auto-generated methot stub
+
+                                              Intent i = new Intent (getApplicationContext(), Tentang.class);
+                                              startActivity(i);
+                                          }
+                                      }
+        );
+
+        btnPeriksa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View argo) {
+                //TODO Auto-generated methot stub
+
+                Intent j = new Intent (getApplicationContext(), Periksa.class);
+                startActivity(j);
+            }
+        }
+        );
+        btnHasil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View argo) {
+                //TODO Auto-generated methot stub
+
+                Intent k = new Intent (getApplicationContext(), Hasil.class);
+                startActivity(k);
+            }
+        }
+        );
     }
 }
