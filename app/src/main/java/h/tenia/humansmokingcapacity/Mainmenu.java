b41ceeac34360501702434cc44b664ec1b596d6a
Tenia <tenia.rosalina@gmail.com>
2018-08-24 02:31:29 -0700

    initial commit

---
+++ app/src/main/java/h/tenia/humansmokingcapacity/Mainmenu.java
@@ -0,0 +1,13 @@
+package h.tenia.humansmokingcapacity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mainmenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainmenu);
+    }
+}
---
+++ app/src/main/java/h/tenia/humansmokingcapacity/SplashScreenActivity.java
@@ -0,0 +1,18 @@
+package h.tenia.humansmokingcapacity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, Mainmenu.class);
+        startActivity(intent);
+        finish();
+    }
+}
