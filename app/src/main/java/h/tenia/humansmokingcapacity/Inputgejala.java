638e9324186dca55d14ffa80dae56bba3cdf0249
Tenia <tenia.rosalina@gmail.com>
2018-08-24 19:14:14 -0700

    initial commit

--- app/src/main/java/h/tenia/humansmokingcapacity/Inputgejala.java
+++ app/src/main/java/h/tenia/humansmokingcapacity/Inputgejala.java
@@ -1,8 +1,12 @@
 package h.tenia.humansmokingcapacity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class Inputgejala extends Activity {
 
@@ -10,5 +14,19 @@ public class Inputgejala extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inputgejala);
+        final LinearLayout btnlihanhasil = (LinearLayout) findViewById(R.id.btnlihathasil);
+
+        btnlihanhasil.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View argo) {
+                                              //TODO Auto-generated methot stub
+
+                                              Intent m = new Intent (getApplicationContext(), Hasil.class);
+                                              startActivity(m);
+                                          }
+                                      }
+
+        );
+
     }
 }
