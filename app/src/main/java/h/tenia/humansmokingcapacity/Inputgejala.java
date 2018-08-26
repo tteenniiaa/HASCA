f7aebe5c4da8cff8829ae0fc25d11f92bb1aec14
Tenia <tenia.rosalina@gmail.com>
2018-08-26 10:47:39 -0700

    initial commit

--- app/src/main/java/h/tenia/humansmokingcapacity/Inputgejala.java
+++ app/src/main/java/h/tenia/humansmokingcapacity/Inputgejala.java
@@ -6,20 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 
 public class Inputgejala extends Activity {
     ArrayList<String> Selection = new ArrayList<String>();
-
+    TextView final_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inputgejala);
-
+        final_text = (TextView) findViewById(R.id.tvhasilgejala);
+        final_text.setEnabled(false);
 
         final LinearLayout btnlihanhasil = (LinearLayout) findViewById(R.id.btnlihathasil);
 
@@ -38,10 +41,33 @@ public class Inputgejala extends Activity {
     }
 
     public void SelectItem(View view) {
-
-
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+            case R.id.CBGP01:
+                if (checked) {
+                    Selection.add("Batuk terus menerus atau semakin parahh");}
+                else
+                {
+                    Selection.remove("Batuk terus menerus atau semakin parah");
+                }
+                break;
+            case R.id.CBGP02:
+                if (checked) {
+                    Selection.add("Batuk lebih dari 2 minggu");}
+                else
+                {
+                    Selection.remove("Batuk lebih dari 2 minggu");
+                }
+                break;
+        }
     }
-
     public void FinalSelection(View view) {
+        String final_gejala_selection = "";
+            for(String Selections : Selection)
+            {
+                final_gejala_selection = final_gejala_selection + Selections + "\n";
+            }
+                final_text.setText(final_gejala_selection);
+                final_text.setEnabled(true);
     }
 }
